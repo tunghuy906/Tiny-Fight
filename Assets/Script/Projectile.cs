0643389050d495fd556ab8239d857b3ee8a59047
seotow <trunghieu152004@gmail.com>
2025-04-08 20:18:47 +0700

    Enhance boss movement by implement FSM

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -8,19 +8,34 @@ public class Projectile : MonoBehaviour
     public int damage = 10;
     public Vector2 moveSpeed = new Vector2(10f, 0);
 	public Vector2 knockback = new Vector2(0, 0);
+    public float maxDistance = 30f; // Khoảng cách tối đa viên đạn có thể di chuyển
+    private Vector2 startPosition;
 
     Rigidbody2D rb;
 
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+
+		// Gán tag "Projectile" cho viên đạn
+		gameObject.tag = "Projectile";
 	}
 
 	// Start is called before the first frame update
-	void Start()
+	private void Start()
     {
+		startPosition = transform.position;
 		rb.velocity = new Vector2(moveSpeed.x * transform.localScale.x, moveSpeed.y);
     }
+
+	private void Update()
+	{
+		// Hủy viên đạn nếu vượt quá khoảng cách tối đa
+		if (Vector2.Distance(startPosition, transform.position) >= maxDistance)
+		{
+			Destroy(gameObject);
+		}
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
@@ -39,4 +54,3 @@ public class Projectile : MonoBehaviour
 		}
 	}
 }
-                                       
--- Assets/Script/ProjectilePrefab.cs
+++ Assets/Script/ProjectilePrefab.cs
@@ -9,13 +9,16 @@ public class ProjectilePrefab : MonoBehaviour
 
     public void FireProjectile()
     {
-        GameObject projectile =  Instantiate(projectilePrefab, launchPoint.position, projectilePrefab.transform.rotation);
+        GameObject projectile = Instantiate(projectilePrefab, launchPoint.position, projectilePrefab.transform.rotation);
         Vector3 origScale = projectile.transform.localScale;
 
         projectile.transform.localScale = new Vector3(
             origScale.x * transform.localScale.x > 0 ? 0.1f : -0.1f,
             origScale.y,
             origScale.z
-            );
+        );
+
+        // Gán tag "Projectile" cho viên đạn
+        projectile.tag = "Projectile";
     }
 }
