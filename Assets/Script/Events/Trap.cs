3bd2c4c1aff7f95927cba24e304b2044aad780d2
seotow <trunghieu152004@gmail.com>
2025-04-08 11:26:16 +0700

    Update UI and create Map 1
    - Health bar + energy bar
    - Loot box: Health and temporary increase movement speed
    - Boss map 1

---
+++ Assets/Script/Events/Trap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+	public int damage = 10; // Sát thương khi chạm vào gai
+	public Vector2 knockbackForce = new Vector2(0, 5); // Lực đẩy ngược lại khi va chạm
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player")) // Kiểm tra nếu đối tượng va chạm là Player
+		{
+			Damageable damageable = other.GetComponent<Damageable>();
+			if (damageable != null)
+			{
+				bool wasHit = damageable.Hit(damage, knockbackForce); // Trừ máu và đẩy lùi
+				if (wasHit)
+				{
+					Debug.Log("Player bị gai đâm! -10 HP");
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public PlayerController player;
+	public static GameManager instance;
+	public Ui_User uiUser;
+
+	[Header("SkillPoint info")]
+	public int skillPoints;
+
+	[Header("UI Elements")]
+	public GameObject deathPanel;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public void UnlockPlayer() => player.playerUnlocked = true;
+	public void RestartLevel(){
+		SceneManager.LoadScene(0);
+		SaveInfo();
+	}
+	public void SaveInfo()
+	{
+		int savedSkillPoint = PlayerPrefs.GetInt("SkillPoints");
+
+		PlayerPrefs.SetInt("SkillPoint", savedSkillPoint + skillPoints);
+	}
+	public void ShowDeathUI()
+	{
+		deathPanel.SetActive(true);
+	}
+	public void AddSkillPoint(int amount)
+	{
+		skillPoints += amount;
+		uiUser.IncreaseSkillPoint(amount);
+	}
+}
